69553122945354df148d1bc886474ff794522263
Administrator <3531962676@qq.com>
2023-06-06 11:33:13 +0800

    chire

--- src/chire/ChireJavaMod.java
+++ src/chire/ChireJavaMod.java
@@ -2,31 +2,43 @@ package chire;
 
 import arc.Core;
 import arc.Events;
+import arc.files.Fi;
+import arc.func.Boolp;
+import arc.func.Cons;
+import arc.func.Floatc;
+import arc.func.Intc;
 import arc.graphics.g2d.TextureRegion;
 import arc.scene.style.Drawable;
 import arc.scene.ui.Button;
 import arc.scene.ui.layout.Table;
 import arc.util.Log;
+import arc.util.OS;
 import arc.util.Time;
 import chire.content.*;
 import chire.ui.CRJoinDialog;
 import mindustry.Vars;
 import mindustry.content.Planets;
+import mindustry.core.Version;
 import mindustry.game.EventType;
 import mindustry.game.EventType.ClientLoadEvent;
 import mindustry.gen.Icon;
+import mindustry.graphics.Pal;
 import mindustry.mod.Mod;
+import mindustry.ui.Bar;
 import mindustry.ui.MobileButton;
 import mindustry.ui.Styles;
 import mindustry.ui.dialogs.BaseDialog;
 import mindustry.ui.dialogs.SettingsMenuDialog;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Objects;
 
-import static arc.Core.atlas;
 import static chire.util.CoreCache.*;
-import static mindustry.Vars.state;
-import static mindustry.Vars.ui;
+import static mindustry.Vars.*;
 
 public class ChireJavaMod extends Mod{
     private String searchtxt;
@@ -76,6 +88,29 @@ public class ChireJavaMod extends Mod{
                     }).size(100f, 50f);
                     dialog1.cont.button("清除", () -> {
                         remove(keytxt);
+                    }).size(100f, 50f).row();
+                    dialog1.cont.button("下载", () -> {
+                        boolean[] cancel = {false};
+                        float[] progress = {0};
+                        int[] length = {0};
+                        Fi file = bebuildDirectory.child("asa.mp4");
+                        String updateUrl = "http://192.168.0.105:5244/d/%E6%88%91%E7%9A%84%E7%A1%AC%E7%9B%98/%E8%A7%86%E9%A2%91/%E5%85%B6%E4%BB%96/A%E7%BA%A7%E9%95%BF.mp4";
+
+                        BaseDialog dialogdown = new BaseDialog("@be.updating");
+                        download(updateUrl, file, i -> length[0] = i, v -> progress[0] = v, () -> cancel[0], () -> {
+
+                        }, ed -> {
+                            dialogdown.hide();
+                            ui.showException(ed);
+                        });
+
+                        dialogdown.cont.add(new Bar(() -> length[0] == 0 ? Core.bundle.get("be.updating") : (int)(progress[0] * length[0]) / 1024/ 1024 + "/" + length[0]/1024/1024 + " MB", () -> Pal.accent, () -> progress[0])).width(400f).height(70f);
+                        dialogdown.buttons.button("@cancel", Icon.cancel, () -> {
+                            cancel[0] = true;
+                            dialogdown.hide();
+                        }).size(210f, 64f);
+                        dialogdown.setFillParent(false);
+                        dialogdown.show();
                     }).size(100f, 50f).row();
 
                     dialog1.cont.button("退出", dialog1::hide).size(100f, 50f);
@@ -163,7 +198,32 @@ public class ChireJavaMod extends Mod{
         return "[" + ModName + "] " + info;
     }
     public static TextureRegion Load(String name){
-        return atlas.find(name);
+        return Core.atlas.find(name);
     }
 
+    private void download(String furl, Fi dest, Intc length, Floatc progressor, Boolp canceled, Runnable done, Cons<Throwable> error) {
+        mainExecutor.submit(() -> {
+            try {
+                HttpURLConnection con = (HttpURLConnection) new URL(furl).openConnection();
+                BufferedInputStream in = new BufferedInputStream(con.getInputStream());
+                OutputStream out = dest.write(false, 4096);
+
+                byte[] data = new byte[4096];
+                long size = con.getContentLength();
+                long counter = 0;
+                length.get((int) size);
+                int x;
+                while ((x = in.read(data, 0, data.length)) >= 0 && !canceled.get()) {
+                    counter += x;
+                    progressor.get((float) counter / (float) size);
+                    out.write(data, 0, x);
+                }
+                out.close();
+                in.close();
+                if (!canceled.get()) done.run();
+            } catch (Throwable e) {
+                error.get(e);
+            }
+        });
+    }
 }
